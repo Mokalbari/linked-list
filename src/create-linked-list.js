2c79f7ce6c6ee5469c0af35443d0c4253845f8b6
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-12-17 11:21:56 +0100

    add methods to linked list

--- src/create-linked-list.js
+++ src/create-linked-list.js
@@ -9,29 +9,88 @@ export function createLinkedList() {
     head = createNode(value, head)
   }
 
-  const getFirst = () => {
-    if (!head) return null
+  const getHead = () => {
+    if (isEmpty()) return null
     return head.data
   }
 
   const setLast = (value) => {
-    if (!head) createNode(value, head)
-    let tempHead = head.next
-    while (tempHead.next !== null) {
-      tempHead = tempHead.next
+    const newNode = createNode(value)
+
+    if (isEmpty()) {
+      head = newNode
     }
-    tempHead.next = createNode(value)
+
+    let currentNode = head
+
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next
+    }
+    currentNode.next = newNode
   }
 
-  const getLast = () => {
-    if (!head) return null
+  const getTail = () => {
+    if (isEmpty()) return null
 
-    let tempHead = head.next
-    while (tempHead.next !== null) {
-      tempHead = tempHead.next
+    let currentNode = head
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next
     }
-    return tempHead
+    return currentNode.data
   }
 
-  return { isEmpty, setFirst, getFirst, setLast, getLast }
+  const getSize = () => {
+    let size = 0
+    if (isEmpty()) return size
+
+    let currentNode = head
+    while (currentNode !== null) {
+      size++
+      currentNode = currentNode.next
+    }
+    return size
+  }
+
+  const getDataAtIndex = (index) => {
+    let currentIndex = 0
+    if (isEmpty()) return null
+
+    let currentNode = head
+    while (currentIndex < index) {
+      if (currentIndex === index) return currentNode.data
+      currentNode = currentNode.next
+      currentIndex++
+    }
+    return null
+  }
+
+  const pop = () => {
+    if (isEmpty()) return null
+
+    if (head.next === null) {
+      const data = head.data
+      head = null
+      return data
+    }
+
+    let currentNode = head
+    while (currentNode.next.next !== null) {
+      currentNode = currentNode.next
+    }
+
+    const data = currentNode.next.data
+    currentNode.next = null
+    return data
+  }
+
+  return {
+    isEmpty,
+    setFirst,
+    getHead,
+    setLast,
+    getTail,
+    getSize,
+    getDataAtIndex,
+    pop,
+  }
 }
---
+++ src/tests/link-list.ts
@@ -0,0 +1,2 @@
+import { describe } from "jsr:@std/testing/bdd"
+describe("linked list", () => {})
